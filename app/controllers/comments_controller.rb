35f30985c7d1621f6e368fa7eddbd7e3cf6bfcf5
Levi Adler <leviadler@gmail.com>
2014-06-05 23:20:37 -0400

    refactor not to use n+1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,19 @@
 class CommentsController < ApplicationController
-  
+
   before_action :require_signed_in
-  
+
   def new
     @comment = Comment.new(parent_comment_id: params[:comment_id],
                             post_id: params[:post_id])
 
   end
-  
+
   def create
     @comment = current_user.comments.new(comment_params)
-    @comment.post_id = @comment.parent_comment.post_id
-    
+    if @comment.post_id.nil?
+      @comment.post_id = @comment.parent_comment.post_id
+    end
+
     if @comment.save
       flash[:notice] = "Comment Added"
       redirect_to post_url(@comment.post)
@@ -20,7 +22,7 @@ class CommentsController < ApplicationController
       render :new
     end
   end
-  
+
   private
   def comment_params
     params.require(:comment).permit(:content, :post_id, :parent_comment_id)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,34 +1,34 @@
 class PostsController < ApplicationController
-  
+
   before_action :require_signed_in, only: [:new, :create, :edit, :update]
-  
+
   helper_method :permitted_to_edit?
-  
+
   def new
     @post = Post.new
   end
-  
+
   def create
     @post = current_user.posts.new(post_params.merge({sub_id: params[:sub_id]}))
-    
+
     if @post.save
       redirect_to post_url(@post)
     else
       flash.now[:errors] = @post.errors.full_messages
       render :new
     end
-    
+
   end
-  
+
   def edit
     @post = Post.find(params[:id])
     require_submitter_or_moderator
   end
-  
+
   def update
     @post = Post.find(params[:id])
     require_submitter_or_moderator
-    
+
     if @post.update(post_params)
       redirect_to post_url(@post)
     else
@@ -36,28 +36,37 @@ class PostsController < ApplicationController
       render :edit
     end
   end
-  
-  
+
+
   def show
     @post = Post.find(params[:id])
     @all_comments = Comment.where(post_id: @post.id).includes(:submitter)
+    @all_comments_hash = hashify_comments(@all_comments)
   end
-  
+
   private
   def post_params
     params.require(:post).permit(:title, :url, :content)
   end
-  
+
   def require_submitter_or_moderator
     unless permitted_to_edit?(@post)
       flash[:notice] = "You cannot edit this post"
       redirect_to post_url(@post)
     end
   end
-  
+
   def permitted_to_edit?(post)
     post.submitter == current_user || post.sub.moderator == current_user
   end
-    
+
+  def hashify_comments(comments)
+    comments_hash = Hash.new(Array.new)
+    comments.each do |comment|
+      comments_hash[comment.parent_comment_id] << comment
+    end
+    comments_hash
+  end
+
 
 end
